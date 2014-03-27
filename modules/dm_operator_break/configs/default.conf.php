5cb463ddfde40f17ea9f6a9428fdecc5d7824138
Supme <supmea@gmail.com>
2014-03-27 17:16:45 +0400

    Переименование для единообразия

--- modules/dm_operator_break/configs/default.conf.php
+++ modules/dm_operator_break/configs/default.conf.php
@@ -29,7 +29,7 @@
     global $arrConf;
     global $arrConfModule;
 
-    $arrConfModule['module_name']       = 'operator_break';
+    $arrConfModule['module_name']       = 'dm_operator_break';
     $arrConfModule['templates_dir']     = 'themes';
     //ex1: $arrConfModule['dsn_conn_database'] = "sqlite3:///$arrConf[elastix_dbdir]/base_name.db";
     //ex2: $arrConfModule['dsn_conn_database'] = "mysql://user:password@ip_host_sever_mysql/base_name";
