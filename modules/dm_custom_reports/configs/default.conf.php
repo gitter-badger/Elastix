420fae2495f85e80fdd36a00f17e9ff97298815d
Supme <supmea@gmail.com>
2014-03-27 17:16:00 +0400

    Переименование для единообразия

--- modules/dm_custom_reports/configs/default.conf.php
+++ modules/dm_custom_reports/configs/default.conf.php
@@ -29,7 +29,7 @@
     global $arrConf;
     global $arrConfModule;
 
-    $arrConfModule['module_name']       = 'custom_reports';
+    $arrConfModule['module_name']       = 'dm_custom_reports';
     $arrConfModule['templates_dir']     = 'themes';
     //ex1: $arrConfModule['dsn_conn_database'] = "sqlite3:///$arrConf[elastix_dbdir]/base_name.db";
     //ex2: $arrConfModule['dsn_conn_database'] = "mysql://user:password@ip_host_sever_mysql/base_name";
